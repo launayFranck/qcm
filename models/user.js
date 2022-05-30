e76baf7593c78c85d726e7cb53d51eb771ddd173
NaorimSenchai <romainchenais35@gmail.com>
2022-05-30 13:02:19 +0200

    Added user route template

---
+++ models/user.js
@@ -0,0 +1,39 @@
+import knex from './models/knexClient';
+
+const findAll = async () => {
+
+}
+
+const findById = async (id) => {
+
+}
+
+const findByUsername = async (username) => {
+
+}
+
+const findByEmail = async (email) => {
+
+}
+
+const create = async (payload) => {
+
+}
+
+const update = async (payload) => {
+
+}
+
+const destroy = async (email) => {
+
+}
+
+export {
+    findAll,
+    findById,
+    findByUsername,
+    findByEmail,
+    create,
+    update,
+    destroy
+}
