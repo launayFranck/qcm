9f794104161d95080e3b9f557412c3259bfa6441
NaorimSenchai <romainchenais35@gmail.com>
2022-05-31 13:18:42 +0200

    Removed plural user

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { authenticateToken } from '../middleware/authorization.js';
 
-import users from '../models/user.js';
+import user from '../models/user.js';
 
 const router = express.Router();
 
 // • Fetching all users
 router.get('/', async (req, res) => {
 	try {
-		const result = await users.findAll();
+		const result = await user.findAll();
 		res.status(200).json({users : result});
 	} catch (err) {
 		res.status(500).json({error : err.message});
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 // • Fetching user by id
 router.get('/:id', async (req, res) => {
 	try {
-		const result = await users.findById(req.params.id);
+		const result = await user.findById(req.params.id);
 		res.status(200).json({users : result});
 	} catch (err) {
 		res.status(500).json({error : err.message});
@@ -28,7 +28,7 @@ router.get('/:id', async (req, res) => {
 // • Fetching user by username
 router.get('/username/:username', async (req, res) => {
 	try {
-		const result = await users.findByUsername(req.params.username);
+		const result = await user.findByUsername(req.params.username);
 		res.status(200).json({users : result});
 	} catch (err) {
 		res.status(500).json({error : err.message});
@@ -38,7 +38,7 @@ router.get('/username/:username', async (req, res) => {
 // • Fetching user by email
 router.get('/email/:email', async (req, res) => {
 	try {
-		const result = await users.findByEmail(req.body.email);
+		const result = await user.findByEmail(req.body.email);
 		res.status(200).json({users : result});
 	} catch (err) {
 		res.status(500).json({error : err.message});
@@ -48,7 +48,7 @@ router.get('/email/:email', async (req, res) => {
 // • Creating new user
 router.post("/", async (req, res) => {
 	try {
-		const result = await users.create(req.body);
+		const result = await user.create(req.body);
 		res.status(200).json({users : result});
 	} catch (err) {
 		res.status(500).json({error : err.message});
@@ -58,7 +58,7 @@ router.post("/", async (req, res) => {
 // • Updating user
 router.put('/:id', async (req, res) => {
 	try {
-		const response = await users.update(req.params.id, req.body);
+		const response = await user.update(req.params.id, req.body);
 		res.status(200).json(response);
 	} catch (err) {
 		res.status(400).json({error : err.message});
@@ -68,7 +68,7 @@ router.put('/:id', async (req, res) => {
 // • Deleting user
 router.delete('/:id', async (req, res) => {
 	try {
-		const response = await users.destroy(req.params.id);
+		const response = await user.destroy(req.params.id);
 		res.status(200).json(response);
 	} catch (err) {
 		res.status(400).json(err.message);
