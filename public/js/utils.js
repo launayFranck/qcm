fec4031b97ed082d872d7f17260e15fb757a161a
NaorimSenchai <romainchenais35@gmail.com>
2022-06-27 12:05:35 +0200

    Strongly updated capitalize and formatDate functions

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,23 +1,77 @@
 /**
  * Capitalizes a string
  * @param {string} str The string to capitalize
+ * @param {boolean} capitalizeAllWords Defines if each word from the string must be capitalized
  * @returns {string} The capitalized string
  */
-const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+const capitalize = (str, capitalizeAllWords = false) => {
+	const cap = (s) => s.charAt(0).toUpperCase() + s.slice(1);
+
+	if (capitalizeAllWords) return str.split(' ').map(str => cap(str)).join('');
+	else return cap(str);
+};
 
 /**
  * Turns a timestamp into a formatted date string
  * @param {date} timestamp The timestamp we wish to convert
- * @returns {string} A date with the following format : DD/MM/YYYY
+ * @param {string} format The string to use as a template for the formatted date. Can include $Y (year), $M (month), $D (date), $d (day), $H (hours), $m (minutes), $s (seconds). E.g. : "$Y/$M/$D at $H/$m".
+ * @returns {string} A date with the following format : "DD/MM/YYYY"
  */
-const formatDate = (timestamp) => {
+const formatDate = (timestamp, format = '$d $D/$M/$Y à $H:$m') => {
 	const formatted = new Date(timestamp);
 
 	const year = formatted.getFullYear();
 	const month = ((formatted.getMonth()).toString()).length < 2 ? `0${formatted.getMonth()}` : formatted.getMonth();
 	const date = ((formatted.getDate()).toString()).length < 2 ? `0${formatted.getDate()}` : formatted.getDate();
+	const day = (() => {
+		const lang = (document.querySelector('html').getAttribute('lang')).toUpperCase();
+		const days = {
+			EN : {
+				0 : 'Sunday',
+				1 : 'Monday',
+				2 : 'Tuesday',
+				3 : 'Wednesday',
+				4 : 'Thursday',
+				5 : 'Friday',
+				6 : 'Saturday',
+				default : 'Unknown day'
+			},
+			FR : {
+				0 : 'Dimanche',
+				1 : 'Lundi',
+				2 : 'Mardi',
+				3 : 'Mercredi',
+				4 : 'Jeudi',
+				5 : 'Vendredi',
+				6 : 'Samedi',
+				default : 'Jour inconnu'
+			}
+		};
+		
+		return days[lang] ?
+			days[lang][formatted.getDay()] ?
+				days[lang][formatted.getDay()]
+				:
+				days[lang].default
+			:
+			days.EN.default
+		;
+	})();
+	
+	const hours = ((formatted.getHours()).toString()).length < 2 ? `0${formatted.getHours()}` : formatted.getHours();
+	const minutes = ((formatted.getMinutes()).toString()).length < 2 ? `0${formatted.getMinutes()}` : formatted.getMinutes();
+	const seconds = ((formatted.getSeconds()).toString()).length < 2 ? `0${formatted.getSeconds()}` : formatted.getSeconds();
 
-	return `${date}/${month}/${year}`;
+	let tmp = format;
+	tmp = tmp.replace('$Y', year);
+	tmp = tmp.replace('$M', month);
+	tmp = tmp.replace('$D', date);
+	tmp = tmp.replace('$d', day);
+	tmp = tmp.replace('$H', hours);
+	tmp = tmp.replace('$m', minutes);
+	tmp = tmp.replace('$s', seconds);
+
+	return tmp;
 };
 
 /**
@@ -43,8 +97,41 @@ const sendMessageToPanel = async (msg, color) => {
 	}, 5000);
 };
 
+/**
+ * Sorts an array of objects by one of the objects' property
+ * @param {array} array The array containing objects to sort
+ * @param {string} property The name of the property to use as a filter
+ * @param {boolean} ascending Specifies the order of the required informations
+ * @returns {Array<object>} The sorted array
+ */
+ const sortByProperty = (array, property, ascending = true) => {
+	try {
+		const res = array.sort((a, b) => (a[property].toLowerCase() > b[property].toLowerCase() ?
+			(ascending ? 1 : -1)
+			:
+			(ascending ? -1 : 1)
+		));
+		// const res = array.sort((a, b) => {
+		// 	const aX = Array.isArray(a[property]) ? a[property].length : a[property].toLowerCase();
+		// 	const bX = Array.isArray(b[property]) ? b[property].length : b[property].toLowerCase();
+
+		// 	const result = aX > bX ? (ascending ? 1 : -1) : (ascending ? -1 : 1);
+			
+		// 	console.log(aX, bX, result);
+		// 	return result;
+		// });
+
+		return res;
+	} catch (err) {
+		console.error(err.message);
+		return array;
+	};
+};
+
 export {
     capitalize,
     formatDate,
-    sendMessageToPanel
+    sendMessageToPanel,
+	sortByProperty
 };
+
